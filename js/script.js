19275969da8ea824c4a1722daff61a58eaae7826
Gabriel Santos <mfkrgabriel@gmail.com>
2023-04-21 12:26:11 +0300

    Splitting and renaming functions, formating, change unmutable variables to const

--- js/script.js
+++ js/script.js
@@ -1,87 +1,115 @@
-function loadMenu() {
+function appendListToHTML(listHTML) {
+  document.getElementById("menu").innerHTML = listHTML
+  document.body.style.cursor = "pointer"
+}
 
-  // Const
-  const PROXY = false;
+function handleProxyResponse(data) {
+  const parser = new DOMParser()
+  const htmlData = parser.parseFromString(data.contents, "text/xml")
+  const items = htmlData.getElementsByTagName("item")
+  let list = "<ol>"
 
-  if( PROXY ) {
-  // Proxy access to the RSS feed
-  fetch(
-    `https://api.allorigins.win/get?url=${encodeURIComponent(
-      "https://www.tabnews.com.br/recentes/rss"
-    )}`
-  )
+  for (var i = 0; i < items.length; i++) {
+    const title = items[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
+    const link = items[i].getElementsByTagName("link")[0].childNodes[0].nodeValue
+
+    list += "<li><a href='" + link + "' target='_blank'>" + title + "</a></li>"
+  }
+
+  list += "</ol>"
+
+  appendListToHTML(list)
+}
+
+function handleCommonResponse(items) {
+  var list = "<ol>"
+
+  for (var i = 0; i < items.length; i++) {
+    const { owner_username, tabcoins, title, slug } = items[i]
+    const link = `${URL}/${owner_username}/${slug}`
+
+    const isTrending = tabcoins > 10 ? ' <span class="burning" title="Em alta">🔥</span>' : ""
+    list += "<li><a href='" + link + "' target='_blank'>" + isTrending + " " + title + "</a></li>"
+  }
+
+  list += "</ol>"
+
+  appendListToHTML(list)
+}
+
+function handleFetch(URL, responseHandler) {
+  fetch(URL)
     .then((response) => {
-      if (response.ok) return response.json();
-      throw new Error("Network response was not ok.");
+      if (response.ok) return response.json()
+      throw new Error("Network response was not ok.")
     })
-    .then((data) => {
-      // Parse string to HTML
-      var parser = new DOMParser();
-      var htmlData = parser.parseFromString(data.contents, "text/xml");
+    .then((data) => responseHandler(data))
+    .catch((err) => console.error(err))
+}
+function appendListToHTML(listHTML) {
+  document.getElementById("menu").innerHTML = listHTML
+  document.body.style.cursor = "pointer"
+}
 
-      // Get all items
-      var items = htmlData.getElementsByTagName("item");
-      // Generate OL List
-      var list = "<ol>";
-      // Loop through items
-      for (var i = 0; i < items.length; i++) {
-        // Get title
-        var title = items[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-        // Get link
-        var link = items[i].getElementsByTagName("link")[0].childNodes[0].nodeValue;
-        // Generate list item
-        list += " <li><a href='" + link + "' target='_blank'>" + title + "</a></li>";
-      }
-      // Close list
-      list += "</ol>";
-      
-      // Add menu list
-      document.getElementById("menu").innerHTML = list;
-      
-      // Change body cursor to pointer
-      document.body.style.cursor = "pointer";
-    });
+function handleProxyResponse(data) {
+  const parser = new DOMParser()
+  const htmlData = parser.parseFromString(data.contents, "text/xml")
+  const items = htmlData.getElementsByTagName("item")
+  let list = "<ol>"
+
+  for (var i = 0; i < items.length; i++) {
+    const title = items[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
+    const link = items[i].getElementsByTagName("link")[0].childNodes[0].nodeValue
+
+    list += "<li><a href='" + link + "' target='_blank'>" + title + "</a></li>"
+  }
+
+  list += "</ol>"
+
+  appendListToHTML(list)
+}
+
+function handleCommonResponse(items) {
+  var list = "<ol>"
+
+  for (var i = 0; i < items.length; i++) {
+    const { owner_username, tabcoins, title, slug } = items[i]
+    const link = `${URL}/${owner_username}/${slug}`
+
+    const isTrending = tabcoins > 10 ? '<span class="burning" title="Em alta">🔥</span>' : ""
+    list += "<li><a href='" + link + "' target='_blank'>" + isTrending + " " + title + "</a></li>"
+  }
+
+  list += "</ol>"
+
+  appendListToHTML(list)
+}
+
+function handleFetch(URL, responseHandler) {
+  fetch(URL)
+    .then((response) => {
+      if (response.ok) return response.json()
+      throw new Error("Network response was not ok.")
+    })
+    .then((data) => responseHandler(data))
+    .catch((err) => console.error(err))
+}
+
+function loadMenu() {
+  const PROXY = false // Just a question: why the Proxy is a const and why there's an IF for it if it's a const?
+  const baseURL = "https://www.tabnews.com.br"
+
+  if (PROXY) {
+    const URL = `https://api.allorigins.win/get?url=${encodeURIComponent(`${baseURL}/recentes/rss`)}`
+
+    handleFetch(URL, handleProxyResponse)
   } else {
-  // Reading direct TabNews API
+    const URL = `${baseURL}/api/v1/contents`
 
-  // BASE URL
-  const URL = "https://www.tabnews.com.br";
-
-  const options = {method: 'GET'};
-  fetch(URL+'/api/v1/contents', options)
-    .then(response => response.json())
-    .then((items) => {
-      
-      // Generate OL List
-      var list = "<ol>";
-      // Loop through items
-      for (var i = 0; i < items.length; i++) {
-        const { owner_username, tabcoins, title, slug } = items[i];
-
-        // Link
-        var link = `${URL}/${owner_username}/${slug}`;
-
-        // Is burining?
-        const burning = ( tabcoins > 10 ) ? ' <span class="burning" title="Em alta">🔥</span>' : '';  
-
-        // Generate list item
-        list += " <li><a href='" + link + "' target='_blank'>" + burning + " " + title + "</a></li>";
-      }
-      // Close list
-      list += "</ol>";
-      
-      // Add menu list
-      document.getElementById("menu").innerHTML = list;
-      
-      // Change body cursor to pointer
-      document.body.style.cursor = "pointer";
-    })
-    .catch(err => console.error(err));
+    handleFetch(URL, handleCommonResponse)
   }
 }
 
-// Document ready
 document.addEventListener("DOMContentLoaded", function () {
-  // Load menu
-  loadMenu();
-});
+  loadMenu()
+})
